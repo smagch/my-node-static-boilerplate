f4fb05d984f31340127bdae561d7c56eb034f141
smagch <smagch@gmail.com>
2012-06-30 17:10:21 +0900

    add middleware

--- dev.js
+++ dev.js
@@ -10,9 +10,31 @@ var app = module.exports = express();
 
 // Configuration
 var publicPath = require('path').resolve('.', 'public');
+var path = require('path')
+  , exec = require('child_process').exec;
+
+function execMake(filename, next) {
+  exec('make ' + filename + ' -B', function (err, stderr, stdout) {
+    console.log('made : ' + filename);
+    next();
+  });
+};
+
+app.get('*/', function (req, res, next) {
+  execMake('public' + req.url + '.html', next);
+});
+
+app.get('*.html', function (req, res, next) {
+  execMake('public' + req.url, next);
+});
+
+app.get('*.css', function (req, res, next) {
+  execMake('public' + req.url, next);
+});
 
 app.use(express.static(publicPath));
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 
 http.createServer(app).listen(3000, function () {
   console.log('server start');
